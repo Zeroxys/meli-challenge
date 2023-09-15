52fea38122288f85eaa6c602ae4a4ca81a72758b
Miguel Zavala <miguelzavalac@gmail.com>
2023-09-14 18:50:06 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `meli-challenge`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
